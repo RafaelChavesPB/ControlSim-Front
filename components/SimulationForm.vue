7eb3b31f493fd89daa33e48f9fc06bc742bd0fee
rafael <rafaelchavespb@gmail.com>
2023-01-28 10:32:04 -0300

    Adição de tab

--- components/SimulationForm.vue
+++ components/SimulationForm.vue
@@ -1,174 +1,185 @@
 <template>
   <v-card>
-    <v-card-text>
-      <v-form ref="form" @submit.prevent="submitForm">
-        <v-expansion-panels popout>
-          <v-expansion-panel>
-            <v-expansion-panel-header>
-              <h2 class="font-weight-medium">Sistema</h2>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <v-row class="mt-2">
-                <v-col class="py-0 px-1">
-                  <v-text-field
-                    size="small"
-                    label="Ganho"
-                    v-model="system.gain"
-                    :rules="rules.gain"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-              <v-row class="mt-2">
-                <v-col class="py-0 px-1" md="8" xs="4" sm="6">
-                  <v-text-field
-                    size="small"
-                    label="Numerador"
-                    v-model="system.num"
-                    :rules="rules.num"
-                    outlined
-                  />
-                </v-col>
-                <v-col class="py-0 px-2" md="4" xs="8" sm="6">
-                  <v-select
-                    size="small"
-                    label="Tipo"
-                    :items="items"
-                    v-model="system.num_type"
-                    :rules="rules.num_type"
-                    outlined
-                  ></v-select>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col class="py-0 px-1" md="8" xs="4" sm="6">
-                  <v-text-field
-                    label="Denominador"
-                    v-model="system.den"
-                    :rules="rules.den"
-                    outlined
-                  />
-                </v-col>
-                <v-col class="py-0 px-2" md="4" xs="8" sm="6">
-                  <v-select
-                    size="small"
-                    label="Tipo"
-                    :items="items"
-                    v-model="system.den_type"
-                    :rules="rules.den_type"
-                    outlined
-                  ></v-select>
-                </v-col>
-              </v-row>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-          <v-expansion-panel>
-            <v-expansion-panel-header>
-              <h2 class="font-weight-medium">Compensador</h2>
-            </v-expansion-panel-header>
-            <v-expansion-panel-content>
-              <v-row class="mt-2">
-                <v-col class="py-0 px-1">
-                  <v-text-field
-                    size="small"
-                    label="Ganho"
-                    v-model="comp.gain"
-                    :rules="rules.gain"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-              <v-row class="mt-2">
-                <v-col class="py-0 px-1" md="8" xs="4" sm="6">
-                  <v-text-field
-                    size="small"
-                    label="Numerador"
-                    v-model="comp.num"
-                    :rules="rules.num"
-                    outlined
-                  />
-                </v-col>
-                <v-col class="py-0 px-2" md="4" xs="8" sm="6">
-                  <v-select
-                    size="small"
-                    label="Tipo"
-                    :items="items"
-                    v-model="comp.num_type"
-                    :rules="rules.num_type"
-                    outlined
-                  ></v-select>
-                </v-col>
-              </v-row>
-              <v-row>
-                <v-col class="py-0 px-1" md="8" xs="4" sm="6">
-                  <v-text-field
-                    label="Denominador"
-                    v-model="comp.den"
-                    :rules="rules.den"
-                    outlined
-                  />
-                </v-col>
-                <v-col class="py-0 px-2" md="4" xs="8" sm="6">
-                  <v-select
-                    size="small"
-                    label="Tipo"
-                    :items="items"
-                    v-model="comp.den_type"
-                    :rules="rules.den_type"
-                    outlined
-                  ></v-select>
-                </v-col>
-              </v-row>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-          <v-expansion-panel>
-            <v-expansion-panel-header>
-              <h2 class="font-weight-medium">
-                Controlador PID
-              </h2></v-expansion-panel-header
-            >
-            <v-expansion-panel-content>
-              <v-row class="mt-2">
-                <v-col class="py-0 px-1">
-                  <v-text-field
-                    size="small"
-                    label="Kp"
-                    v-model="pid.kp"
-                    :rules="rules.kp"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-              <v-row class="mt-2">
-                <v-col class="py-0 px-1">
-                  <v-text-field
-                    size="small"
-                    label="Kd"
-                    v-model="pid.kd"
-                    :rules="rules.kd"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-              <v-row class="mt-2">
-                <v-col class="py-0 px-1">
-                  <v-text-field
-                    size="small"
-                    label="Ki"
-                    v-model="pid.ki"
-                    :rules="rules.ki"
-                    outlined
-                  />
-                </v-col>
-              </v-row>
-            </v-expansion-panel-content>
-          </v-expansion-panel>
-        </v-expansion-panels>
-      </v-form>
-    </v-card-text>
-    <v-card-actions class="d-flex justify-center">
-      <v-btn type="submit" color="primary" size="big">Compilar</v-btn>
-    </v-card-actions>
+    <v-tabs v-model="tab" align-with-title>
+      <v-tab href="#form"> Gerar simulação </v-tab>
+      <v-tab href="#results"> Resultados </v-tab>
+    </v-tabs>
+    <v-tabs-items v-model="tab" class="mt-5">
+      <v-tab-item value="form">
+        <v-form ref="form" @submit.prevent="submitForm">
+
+          <v-expansion-panels popout>
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                <h2 class="font-weight-medium">Sistema</h2>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row class="mt-2">
+                  <v-col class="py-0 px-1">
+                    <v-text-field
+                      size="small"
+                      label="Ganho"
+                      v-model="system.gain"
+                      :rules="rules.gain"
+                      outlined
+                    />
+                  </v-col>
+                </v-row>
+                <v-row class="mt-2">
+                  <v-col class="py-0 px-1" md="8" xs="4" sm="6">
+                    <v-text-field
+                      size="small"
+                      label="Numerador"
+                      v-model="system.num"
+                      :rules="rules.num"
+                      outlined
+                    />
+                  </v-col>
+                  <v-col class="py-0 px-2" md="4" xs="8" sm="6">
+                    <v-select
+                      size="small"
+                      label="Tipo"
+                      :items="items"
+                      v-model="system.num_type"
+                      :rules="rules.num_type"
+                      outlined
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="py-0 px-1" md="8" xs="4" sm="6">
+                    <v-text-field
+                      label="Denominador"
+                      v-model="system.den"
+                      :rules="rules.den"
+                      outlined
+                    />
+                  </v-col>
+                  <v-col class="py-0 px-2" md="4" xs="8" sm="6">
+                    <v-select
+                      size="small"
+                      label="Tipo"
+                      :items="items"
+                      v-model="system.den_type"
+                      :rules="rules.den_type"
+                      outlined
+                    ></v-select>
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                <h2 class="font-weight-medium">Compensador</h2>
+              </v-expansion-panel-header>
+              <v-expansion-panel-content>
+                <v-row class="mt-2">
+                  <v-col class="py-0 px-1">
+                    <v-text-field
+                      size="small"
+                      label="Ganho"
+                      v-model="comp.gain"
+                      :rules="rules.gain"
+                      outlined
+                    />
+                  </v-col>
+                </v-row>
+                <v-row class="mt-2">
+                  <v-col class="py-0 px-1" md="8" xs="4" sm="6">
+                    <v-text-field
+                      size="small"
+                      label="Numerador"
+                      v-model="comp.num"
+                      :rules="rules.num"
+                      outlined
+                    />
+                  </v-col>
+                  <v-col class="py-0 px-2" md="4" xs="8" sm="6">
+                    <v-select
+                      size="small"
+                      label="Tipo"
+                      :items="items"
+                      v-model="comp.num_type"
+                      :rules="rules.num_type"
+                      outlined
+                    ></v-select>
+                  </v-col>
+                </v-row>
+                <v-row>
+                  <v-col class="py-0 px-1" md="8" xs="4" sm="6">
+                    <v-text-field
+                      label="Denominador"
+                      v-model="comp.den"
+                      :rules="rules.den"
+                      outlined
+                    />
+                  </v-col>
+                  <v-col class="py-0 px-2" md="4" xs="8" sm="6">
+                    <v-select
+                      size="small"
+                      label="Tipo"
+                      :items="items"
+                      v-model="comp.den_type"
+                      :rules="rules.den_type"
+                      outlined
+                    ></v-select>
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+
+            <v-expansion-panel>
+              <v-expansion-panel-header>
+                <h2 class="font-weight-medium">
+                  Controlador PID
+                </h2></v-expansion-panel-header
+              >
+              <v-expansion-panel-content>
+                <v-row class="mt-2">
+                  <v-col class="py-0 px-1">
+                    <v-text-field
+                      size="small"
+                      label="Kp"
+                      v-model="pid.kp"
+                      :rules="rules.kp"
+                      outlined
+                    />
+                  </v-col>
+                </v-row>
+                <v-row class="mt-2">
+                  <v-col class="py-0 px-1">
+                    <v-text-field
+                      size="small"
+                      label="Kd"
+                      v-model="pid.kd"
+                      :rules="rules.kd"
+                      outlined
+                    />
+                  </v-col>
+                </v-row>
+                <v-row class="mt-2">
+                  <v-col class="py-0 px-1">
+                    <v-text-field
+                      size="small"
+                      label="Ki"
+                      v-model="pid.ki"
+                      :rules="rules.ki"
+                      outlined
+                    />
+                  </v-col>
+                </v-row>
+              </v-expansion-panel-content>
+            </v-expansion-panel>
+          </v-expansion-panels>
+
+          <div class="d-flex justify-center my-3">
+            <v-btn type="submit" color="primary" large>Compilar</v-btn>
+          </div>
+
+        </v-form>
+      </v-tab-item>
+    </v-tabs-items>
   </v-card>
 </template>
 
@@ -177,6 +188,7 @@ export default {
   name: "SimulationForm",
   data() {
     return {
+      tab: "",
       items: ["Polinomial", "Racional"],
       system: {
         gain: "1",
